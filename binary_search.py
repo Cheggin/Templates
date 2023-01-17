eb9b25f602b4bdb38619f4a6b53b9eedfaa3b6d0
Reagan Hsu <reaganhsu123@gmail.com>
2023-01-17 12:34:11 -0800

    USACO

--- binary_search.py
+++ binary_search.py
@@ -4,10 +4,10 @@ my_nums = input().strip().split(" ")
 new_nums = []
 for i in nums:
     new_nums.append(int(i))
-new_nums = sorted(new_nums)
-def search(numbers, num):
-    l = 0
-    r = len(nums)-1
+new_nums = sorted(new_nums) #list must be sorted
+def search(numbers, num): 
+    l = 0 #left index
+    r = len(nums)-1 #right index
     count = 0
     indices = ""
     while l <= r:
@@ -18,7 +18,7 @@ def search(numbers, num):
             print("indices: " + str(indices))
             print("count: " + str(count))
             print("number searched: " + str(num))
-            return "found at index: " + str(mid)
+            return str(num) + " found at index: " + str(mid) + "."
         else:
             if int(numbers[mid]) > num:
                 r = mid-1
@@ -26,7 +26,7 @@ def search(numbers, num):
                 l = mid + 1
     print("indices: " + str(indices))
     print("count: " + str(count))        
-    return "num not found: " + str(num)   
+    return str(num)  + " not found."
 
 for i in my_nums:
     print(search(new_nums, int(i)))
---
+++ merge_sort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class merge_sort {
+    public static void main(String[] args){
+        int[] arr1 = new int[]{5, 4, 3, 2, 1};
+        int[] arr2 = new int[]{1, 4, 6, 1, 3};
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int[] sorted = merge(arr1, arr2);
+        for(int i: sorted){
+            System.out.print(i + " ");
+        }
+    }
+    public static int[] merge(int[] a1, int[] a2){
+        int[] sorted = new int[a1.length + a2.length];
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        while(p1 < a1.length && p2 < a2.length){
+            if(a1[p1] < a2[p2]){
+                sorted[p3] = a1[p1];
+                p1++;
+            }
+            else{
+                sorted[p3] = a2[p2];
+                p2++;
+            }
+            p3++;
+        }
+        while(p2 < a2.length){
+            sorted[p3] = a2[p2];
+            p3++;
+            p2++;
+        }
+        while(p1 <a1.length){
+            sorted[p3] = a1[p1];
+            p3++;
+            p1++;
+        }
+        
+    return sorted;
+    }
+} 
