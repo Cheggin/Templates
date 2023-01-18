238dc7401057450f6bc9761ff683cf7fcf592550
Reagan Hsu <reaganhsu123@gmail.com>
2023-01-18 09:26:11 -0800

    COmments

--- insertion_sort.java
+++ insertion_sort.java
@@ -8,9 +8,9 @@ public class insertion_sort {
     }
 
     public static int[] sort(int[] arr){
-        for(int i = 1; i< arr.length; i++){
-            for(int j = i; j >= 1; j--){
-                if(arr[j] < arr[j-1]){
+        for(int i = 1; i< arr.length; i++){ // O(n)
+            for(int j = i; j >= 1; j--){ // O(logn)
+                if(arr[j] < arr[j-1]){ //push jth element to the right place in the leftmost list of j elements
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
